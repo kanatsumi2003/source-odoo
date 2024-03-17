23402b0c468417713051fa1d3deb1e6e64f403e2
sontv <sontv6666@gmail.com>
2024-03-18 00:08:58 +0700

    New module

---
+++ addons/abc1234/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'App View Template And API',
+    'version': '1.2.1',
+    'summary': 'Change login template',
+    'sequence': 30,
+    'description': """
+        Long Description of your modules""",
+    'author': 'Son Bui',
+    'company': 'Cong ty Ma',
+    'website': 'https://www.website.com',
+    'category': 'Customization',
+    'depends': [ 'web','base'],
+    'data': [
+        'views/custom_login_page.xml',
+    ],
+    'application': True,
+     'auto_install': True,
+     'auto_upgrade': True,
+}
